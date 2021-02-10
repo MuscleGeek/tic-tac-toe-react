7cc1dc3dfa72ededa3a97dd47a049a3279fe9a4a
Jonathan Villalobos Mora <hackertristania@gmail.com>
2021-02-10 00:00:48 +0000

    tic tac process

---
+++ src/js/component/board.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import Square from './square.js';
+
+export const Board = (props) => {
+    console.log(props);
+    const renderSquare = (i) => {
+        return (
+            <Square 
+            value={props.squares[i]}
+            onClick={() => props.onClick(i)}>
+            </Square>
+        );
+    };
+    return (
+        <div>
+            <div className="row">
+            {renderSquare(0)}
+            {renderSquare(1)}
+            {renderSquare(2)}
+        </div>
+        <div className="row">
+            {renderSquare(3)}
+            {renderSquare(4)}
+            {renderSquare(5)}
+        </div>
+        <div className="row">
+            {renderSquare(6)}
+            {renderSquare(7)}
+            {renderSquare(8)}
+        </div>
+        </div>
+    );
+}
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,14 @@
 import React from "react";
+import "src/styles/index.scss";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
+import { Square } from "./square.js";
 //create your first component
 export function Home() {
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<Square value="X" onClick={() => alert("X")} />
 		</div>
 	);
 }
